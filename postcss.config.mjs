8ace56ff4170b23c5e7bda1abbf5ed4e82ebe1cf
edisonmbli <edisonmbli@gmail.com>
2025-07-15 00:40:36 +0800

    chapter 1 - debuging css config 2

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,8 @@
 // postcss.config.mjs
-
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    // 1. 使用 @tailwindcss/postcss 插件
     tailwindcss: {},
-    // 2. 自动添加浏览器前缀，增强兼容性
     autoprefixer: {},
   },
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,16 @@
 import type { Config } from 'tailwindcss'
 import animate from 'tailwindcss-animate'
 
-const config = {
+const config: Config = {
   darkMode: 'class',
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  prefix: '',
   theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-      },
-    },
     extend: {
-      fontFamily: {
-        sans: ['var(--font-sans)', 'sans-serif'],
-        serif: ['var(--font-serif)', 'serif'],
-      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
@@ -63,23 +51,9 @@ const config = {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
-      keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
-        },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
-        },
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-      },
     },
   },
   plugins: [animate],
-} satisfies Config
+}
 
 export default config
